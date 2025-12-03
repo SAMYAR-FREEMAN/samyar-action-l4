2d9904a6f11467ef277c3d6e5401b5e4f1cfad1a
SAMYAR-FREEMAN <samyar.n2010@gmail.com>
2025-12-03 21:43:17 +0330

    second commit

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from fastapi import FastApi , HTTPException
 
 app = FastApi()
 
+x = False
+if x:
+    pass
+else:
+    with open("error.txt", "w") as f:
+        f.write("Daaraann mizanannnnnnnn")
+
 @app.get("/")
 def read_root():
     return {"hello": "world"}
