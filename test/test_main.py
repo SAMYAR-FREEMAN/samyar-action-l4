68be6c33dba18c46a6c65a3a1adf0fc1d4699ebe
SAMYAR-FREEMAN <samyar.n2010@gmail.com>
2025-12-03 21:55:19 +0330

    fourth commit

--- test/test_main.py
+++ test/test_main.py
@@ -1,7 +1,7 @@
-from fastapi.testclient import TestClient
-from app import app
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from fastapi.testclient import TestClient
+from app import app
 
 client = TestClient(app)
 
